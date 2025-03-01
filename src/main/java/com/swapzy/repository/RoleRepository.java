ad7ea5ec38d0ec694ff1d617ba5380c72035bc8f
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-03-01 23:32:45 +0530

    Create RoleRepository.java

---
+++ src/main/java/com/swapzy/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.swapzy.repository;
+
+import com.swapzy.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
