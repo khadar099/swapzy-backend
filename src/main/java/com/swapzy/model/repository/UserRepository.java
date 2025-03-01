b0aafe58943307b48a748f666a5b2b04270d1f3e
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-03-02 00:24:24 +0530

    Create UserRepository.java

---
+++ src/main/java/com/swapzy/model/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.swapzy.repository;
+
+import com.swapzy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByUsername(String username);
+}
