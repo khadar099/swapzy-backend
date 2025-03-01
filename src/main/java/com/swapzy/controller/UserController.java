ec249adfc620ad5297fe96b5695c12ec8b3614e4
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-03-01 23:29:25 +0530

    Create UserController.java

---
+++ src/main/java/com/swapzy/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.swapzy.controller;
+
+import com.swapzy.model.User;
+import com.swapzy.repository.UserRepository;
+import com.swapzy.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
