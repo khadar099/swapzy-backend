ea85a49344541ed7fcee9ae04b0ff027a806c796
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-03-01 23:31:53 +0530

    Create Product.java

---
+++ src/main/java/com/swapzy/model/Product.java
@@ -0,0 +1,22 @@
+package com.swapzy.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Getters and Setters
+}
