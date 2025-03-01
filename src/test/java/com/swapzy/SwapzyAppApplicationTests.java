10213ea3d3aa1e98b4eec7695baf6991f8b1b755
khadar <khadar3099@gmail.com>
2025-03-01 23:20:38 +0530

    initial commit

---
+++ src/test/java/com/swapzy/SwapzyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapzyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
