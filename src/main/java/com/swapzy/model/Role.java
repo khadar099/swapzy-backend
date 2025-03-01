f295d3b1a6c7650127f69a248e0e0370f82a5d82
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-03-01 23:31:23 +0530

    Create Role.java

---
+++ src/main/java/com/swapzy/model/Role.java
@@ -0,0 +1,16 @@
+package com.swapzy.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // Getters and Setters
+}
