957f8666b83c521d3864fa34b204286c69d4f033
Khadar Basha <89116722+khadar099@users.noreply.github.com>
2025-03-02 00:24:58 +0530

    Delete src/main/java/com/swapzy/model directory

--- src/main/java/com/swapzy/model/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.swapzy.repository;
-
-import com.swapzy.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
-}
